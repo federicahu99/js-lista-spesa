4bbd760de79561b101ac235fc1f75ae1f93e5069
feddericahu99 <federica.hu339@gmail.com>
2022-06-28 16:34:54 +0200

    while

--- js/script.js
+++ js/script.js
@@ -7,20 +7,22 @@
 let groceryList = '<ul>';
 /* Array */
 const listProduct = [ 'Uova', 'Pane','Latte','Biscotti','Pasta', 'Affettati', 'Formaggio', 'Verdura', 'Frutta']
-//For
-for ( i = 0; i <= listProduct.length -1 ; i++) {
-    groceryList += `<li> ${listProduct[i]} </li>`;
-}
-//Chiudo ul
-groceryList += '</ul>';
-/* Porto nel Dom */
-document.getElementById('grocery').innerHTML= groceryList;
+// //For
+// for ( i = 0; i <= listProduct.length -1 ; i++) {
+//     groceryList += `<li> ${listProduct[i]} </li>`;
+// }
+// //Chiudo ul
+// groceryList += '</ul>';
+// /* Porto nel Dom */
+// document.getElementById('grocery').innerHTML= groceryList;
 
 // Creo un ciclo While
-listProduct[0];
-while (listProduct !== [9]) {
+let i = 0;
+while (i < listProduct.length) {
     groceryList += `<li> ${listProduct[i]} </li>`;
+    console.log(groceryList);
     i++;
 }
 
 groceryList += '</ul>';
+document.getElementById('grocery').innerHTML= groceryList;
